41360718813d53b9e0c9fa2e7b6aa8df26d1a518
Hua Son Tung <tunghuason@gmail.com>
2020-09-20 14:22:17 +0700

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Hua Son Tung on 9/20/20.
+//  Copyright © 2020 Hua Son Tung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
